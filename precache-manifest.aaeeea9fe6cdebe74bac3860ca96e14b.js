a4119f704d9711a34b3324761c9a749a374be1ff
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2024-01-25 20:06:47 +0500

    finnans download button

---
+++ precache-manifest.aaeeea9fe6cdebe74bac3860ca96e14b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3562c6fceccbdee7351",
+    "url": "/css/about.9bf6f21e.css"
+  },
+  {
+    "revision": "f64afe89fd56409da741",
+    "url": "/css/app.570b1df6.css"
+  },
+  {
+    "revision": "dda9345499ba736d0691",
+    "url": "/css/chunk-vendors.1474e5be.css"
+  },
+  {
+    "revision": "e5f18cb987385760e628a9671f975412",
+    "url": "/fonts/Inter-Medium.e5f18cb9.ttf"
+  },
+  {
+    "revision": "851660f90f21dba5ec35b1765fdd426a",
+    "url": "/fonts/Inter-Regular.851660f9.ttf"
+  },
+  {
+    "revision": "a2c4e8821556fa8b48d943a39f9da10c",
+    "url": "/fonts/Inter-SemiBold.a2c4e882.ttf"
+  },
+  {
+    "revision": "233a9c17f35880c1e4e49d58083879a2",
+    "url": "/img/bg35089.233a9c17.png"
+  },
+  {
+    "revision": "262dc3cf28f4718c407f91dbd4c36e2d",
+    "url": "/img/loginlogo.262dc3cf.png"
+  },
+  {
+    "revision": "476b7748eb61440996099a38614aface",
+    "url": "/img/malumot.476b7748.png"
+  },
+  {
+    "revision": "ec7f7adbda39b815355846e48ffb65b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3562c6fceccbdee7351",
+    "url": "/js/about.96376bc7.js"
+  },
+  {
+    "revision": "f64afe89fd56409da741",
+    "url": "/js/app.d93ef183.js"
+  },
+  {
+    "revision": "dda9345499ba736d0691",
+    "url": "/js/chunk-vendors.0c478a42.js"
+  },
+  {
+    "revision": "3b557447ee9a0c9b9f9bfbaaaac83bcb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
